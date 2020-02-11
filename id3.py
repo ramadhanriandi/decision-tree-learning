5040e2609d4abc86f18c56760b4d49f8b7dd7fcb
ramadhanriandi <izurbudianto@gmail.com>
2020-02-11 16:22:09 +0700

    modify class Node

--- id3.py
+++ id3.py
@@ -4,11 +4,28 @@ class Node:
     self.label = label
     self.children = {}
   
-  def setAttribute(attribute):
+  def setAttribute(self, attribute):
     self.attribute = attribute
 
-  def setLabel(label):
+  def setLabel(self, label):
     self.label = label
   
-  def addChildren(attributeValue, node):
+  def addChildren(self, attributeValue, node):
     self.children[attributeValue] = node
+
+
+# Try to build a tree
+n1 = Node('Outlook')
+n2 = Node('Humidity')
+n3 = Node(label = 'Yes')
+n4 = Node('Wind')
+n5 = Node(label = 'Yes')
+n6 = Node(label = 'No')
+
+n1.addChildren('Sunny', n2)
+n1.addChildren('Overcast', n3)
+n1.addChildren('Rain', n4)
+n2.addChildren('High', n5)
+n2.addChildren('Normal', n6)
+
+print(n1.children['Sunny'].children['High'].label)
